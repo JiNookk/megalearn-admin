afba4efd6d2ff5a637f46da70c60b552e2cfb2ba
jingwook <qnfdkf153@gmail.com>
2023-05-05 17:52:08 +0900

    fix: course의 status를 전부 대문자로 변경

--- src/pages/CourseApprovePage.jsx
+++ src/pages/CourseApprovePage.jsx
@@ -1,73 +1,74 @@
 /* eslint-disable react/no-array-index-key */
-import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import Padding from '../components/ui/Padding';
-import useCourseStore from '../hooks/useCourseStore';
-import useLectureStore from '../hooks/useLectureStore';
-import { timeFormat } from '../utils/TimeFormat';
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Padding from "../components/ui/Padding";
+import useCourseStore from "../hooks/useCourseStore";
+import useLectureStore from "../hooks/useLectureStore";
+import { timeFormat } from "../utils/TimeFormat";
 
 const Container = styled.div`
   flex: 1;
-  
-  >div{
+
+  > div {
     height: 100%;
   }
 
-  form{
-    button{
+  form {
+    button {
       width: 100%;
       padding-block: 1rem;
     }
   }
 
-  input{
-    width:100%;
+  input {
+    width: 100%;
     padding: 1rem;
-    margin-bottom: .5rem;
-    border: 1px solid #D3DADD;
+    margin-bottom: 0.5rem;
+    border: 1px solid #d3dadd;
     border-radius: 4px;
   }
-  `;
+`;
 
 const CourseHeader = styled.div`
   border-bottom: 1px solid black;
   margin-block: 1rem;
-  padding-block: .5rem;
-  
-  h2{
+  padding-block: 0.5rem;
+
+  h2 {
     font-size: 1.5rem;
     font-weight: bold;
   }
-  `;
+`;
 
 const Table = styled.table`
   display: block;
   padding-block: 2rem;
 
-  thead, tbody{
+  thead,
+  tbody {
     display: block;
     width: 100%;
   }
 
-  th{
+  th {
     text-align: start;
     font-size: 1.3rem;
     font-weight: 500;
     color: gray;
   }
 
-  tr{
+  tr {
     display: grid;
     grid-template-columns: 40fr 20fr 10fr 10fr 10fr 10fr;
     padding: 1rem;
     align-items: center;
-    border: 1px solid #D3DADD;
+    border: 1px solid #d3dadd;
     border-collapse: collapse;
   }
 
-  td{
-    >div{
+  td {
+    > div {
       display: flex;
       align-items: center;
     }
@@ -89,13 +90,14 @@ const Pagination = styled.ul`
 const Page = styled.button`
   border: none;
   border-radius: 4px;
-  background-color: ${(props) => (props.value === +props.page ? '#dbdedf' : '#FFFFFF')};
+  background-color: ${(props) =>
+    props.value === +props.page ? "#dbdedf" : "#FFFFFF"};
   cursor: pointer;
 `;
 
 export default function CourseApprovePage() {
   const navigate = useNavigate();
-  const currentPage = useLocation().search.split('=')[1];
+  const currentPage = useLocation().search.split("=")[1];
 
   const courseStore = useCourseStore();
   const lectureStore = useLectureStore();
@@ -121,92 +123,76 @@ export default function CourseApprovePage() {
     <Container>
       <Padding>
         <CourseHeader>
-          <h2>
-            강의 관리
-          </h2>
+          <h2>강의 관리</h2>
         </CourseHeader>
         <Table>
           <thead>
             <tr>
-              <th>
-                강의
-              </th>
-              <th>
-                카테고리
-              </th>
-              <th>
-                길이
-              </th>
-              <th>
-                가격
-              </th>
-              <th>
-                난이도
-              </th>
-              <th>
-                상태
-              </th>
+              <th>강의</th>
+              <th>카테고리</th>
+              <th>길이</th>
+              <th>가격</th>
+              <th>난이도</th>
+              <th>상태</th>
             </tr>
           </thead>
           <tbody>
-            {courseStore.courses
-              .map((course) => (
-                <tr key={course.id}>
-                  <td>
-                    <div>
-                      <Image src={course.coverImage || '/assets/test.jpg'} alt="course" />
-                      <p>
-                        {course.title}
-                      </p>
-                    </div>
-                  </td>
-                  <td>
-                    {course.category}
-                  </td>
-                  <td>
-                    {timeFormat.hourMinute(lectureStore.lectures
+            {courseStore.courses.map((course) => (
+              <tr key={course.id}>
+                <td>
+                  <div>
+                    <Image
+                      src={course.coverImage || "/assets/test.jpg"}
+                      alt="course"
+                    />
+                    <p>{course.title}</p>
+                  </div>
+                </td>
+                <td>{course.category}</td>
+                <td>
+                  {timeFormat.hourMinute(
+                    lectureStore.lectures
                       .filter((lecture) => lecture.courseId === course.id)
                       .map((lecture) => lecture.lectureTime)
-                      .reduce((acc, lectureTime) => acc
-                      + 60 * lectureTime.minute + lectureTime.second, 0))}
-                  </td>
-                  <td>
-                    {course.price}
-                  </td>
-                  <td>
-                    {course.level}
-                  </td>
-                  <td>
-                    <form>
-                      <select
-                        defaultValue={course.status}
-                        name="status"
-                        onChange={(e) => handleChangeStatus(e, course.id)}
-                      >
-                        <option value="processing">임시저장</option>
-                        <option value="submitted">제출</option>
-                        <option value="approved">승인</option>
-                      </select>
-                    </form>
-                  </td>
-                </tr>
-              ))}
+                      .reduce(
+                        (acc, lectureTime) =>
+                          acc + 60 * lectureTime.minute + lectureTime.second,
+                        0
+                      )
+                  )}
+                </td>
+                <td>{course.price}</td>
+                <td>{course.level}</td>
+                <td>
+                  <form>
+                    <select
+                      defaultValue={course.status}
+                      name="status"
+                      onChange={(e) => handleChangeStatus(e, course.id)}
+                    >
+                      <option value="PROCESSING">임시저장</option>
+                      <option value="SUBMITTED">제출</option>
+                      <option value="APPROVED">승인</option>
+                    </select>
+                  </form>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
         <Pagination>
-          {[...Array(courseStore.totalPages)]
-            .map((_, page) => (
-              <li key={page}>
-                <Page
-                  type="button"
-                  onClick={() => handleChangePage(page + 1)}
-                  value={page + 1}
-                  page={currentPage}
-                >
-                  {page + 1}
-                </Page>
-              </li>
-            ))}
+          {[...Array(courseStore.totalPages)].map((_, page) => (
+            <li key={page}>
+              <Page
+                type="button"
+                onClick={() => handleChangePage(page + 1)}
+                value={page + 1}
+                page={currentPage}
+              >
+                {page + 1}
+              </Page>
+            </li>
+          ))}
         </Pagination>
       </Padding>
     </Container>
